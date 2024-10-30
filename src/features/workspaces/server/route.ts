d087eb4ada6fec310ac7ea7847dde7884e16d704
Manas0410 <manasshrivastava0410@gmail.com>
2024-10-30 16:11:49 +0530

    api t del workspace

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -150,6 +150,26 @@ const workspaces = new Hono()
 
       return c.json({ data: workspace });
     }
-  );
+  )
+  // delete workspace
+  .delete("/:workspaceId", sessionMiddleware, async (c) => {
+    const { workspaceId } = c.req.param();
+    const databases = c.get("databases");
+    const user = c.get("user");
+
+    const member = await getMember({
+      databases,
+      workspaceId,
+      userId: user.$id,
+    });
+
+    if (!member || member.role !== MemberRole.ADMIN) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+
+    await databases.deleteDocument(DATABASE_ID, WORKSPACES_ID, workspaceId);
+
+    return c.json({ data: { $id: workspaceId } });
+  });
 
 export default workspaces;
