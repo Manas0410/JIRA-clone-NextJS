b4b1c8750160c72123b1444793864f16b5f2cbf2
Manas0410 <manasshrivastava0410@gmail.com>
2024-11-03 17:29:57 +0530

    api to cfreate workspace

--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -1,12 +1,72 @@
-import { DATABASE_ID, PROJECTS_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECTS_ID } from "@/config";
 import { getMember } from "@/features/members/utils";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { z } from "zod";
+import { createProjectSchema } from "../schema";
+import { MemberRole } from "@/features/members/types";
 
 const projects = new Hono()
+
+  // create project
+  .post(
+    "/",
+    sessionMiddleware,
+    zValidator("form", createProjectSchema),
+    async (c) => {
+      const databases = c.get("databases");
+      const storage = c.get("storage");
+      const user = c.get("user");
+
+      const { name, image, workspaceId } = c.req.valid("form");
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      // || member.role !== MemberRole.ADMIN
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      let uploadedImageUrl: string | undefined;
+
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image
+        );
+
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id
+        );
+
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+          arrayBuffer
+        ).toString("base64")}`;
+      }
+
+      const project = await databases.createDocument(
+        DATABASE_ID,
+        PROJECTS_ID,
+        ID.unique(),
+        {
+          name,
+          userid: user.$id,
+          imageUrl: uploadedImageUrl,
+          workspaceId,
+        }
+      );
+
+      return c.json({ data: project });
+    }
+  )
 
   // get all the projects
   .get(
