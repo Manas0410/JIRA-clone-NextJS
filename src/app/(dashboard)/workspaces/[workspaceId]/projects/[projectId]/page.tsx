4174c941a16b152d02c4ffe0e6e4b2e4b4a2b240
Manas0410 <manasshrivastava0410@gmail.com>
2024-11-03 23:43:12 +0530

    loader and error

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -2,7 +2,8 @@ import { Button } from "@/components/ui/button";
 import { getCurrent } from "@/features/auth/queries";
 import { ProjectAvatar } from "@/features/projects/components/projects-avatar";
 import { getProject } from "@/features/projects/queries";
-import { Link, PencilIcon } from "lucide-react";
+import { PencilIcon } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 interface ProjectIdPageProps {
