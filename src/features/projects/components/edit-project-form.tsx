d3b9443db406c9a62d24f7f7bb27984784bec2d3
Manas0410 <manasshrivastava0410@gmail.com>
2024-11-04 22:35:28 +0530

    edit project

---
+++ src/features/projects/components/edit-project-form.tsx
@@ -0,0 +1,266 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { updateProjectSchema } from "../schema";
+import { z } from "zod";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormMessage,
+  FormControl,
+  FormLabel,
+} from "@/components/ui/form";
+import { DottedSeperator } from "@/components/dotted-seperator";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRef } from "react";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import Image from "next/image";
+import { ArrowLeftIcon, CopyIcon, ImageIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Project } from "../types";
+import { useConfirm } from "@/hooks/use-confirm";
+import { toast } from "sonner";
+import { useUpdateProject } from "../api/use-update-project";
+
+interface EditProjectFormProps {
+  onCancel?: () => void;
+  initialValues: Project;
+}
+
+export const EditProjectForm = ({
+  onCancel,
+  initialValues,
+}: EditProjectFormProps) => {
+  const router = useRouter();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const form = useForm<z.infer<typeof updateProjectSchema>>({
+    resolver: zodResolver(updateProjectSchema),
+    defaultValues: {
+      ...initialValues,
+      image: initialValues.imageUrl ?? "",
+    },
+  });
+
+  const { mutate, isPending } = useUpdateProject();
+
+  const [DeleteDialog, ConfirmDelete] = useConfirm(
+    "Delete Workspace",
+    "Are you sure you want to delete this workspace?",
+    "destructive"
+  );
+
+  //   const { mutate: deleteWorkspace, isPending: isDeletePending } =
+  //     useDeleteWorkspace();
+
+  const handleDelete = async () => {
+    // const ok = await ConfirmDelete();
+    // if (!ok) return;
+    // deleteWorkspace(
+    //   {
+    //     param: {
+    //       workspaceId: initialValues.$id,
+    //     },
+    //   },
+    //   {
+    //     onSuccess: () => {
+    //       router.push("/");
+    //     },
+    //   }
+    // );
+  };
+
+  const onSubmit = (values: z.infer<typeof updateProjectSchema>) => {
+    const finalValues = {
+      ...values,
+      image: values.image instanceof File ? values.image : "",
+    };
+    mutate(
+      {
+        form: finalValues,
+        param: {
+          projectId: initialValues.$id,
+        },
+      },
+      {
+        onSuccess: () => {
+          form.reset();
+        },
+      }
+    );
+  };
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      form.setValue("image", file);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      <DeleteDialog />
+      <Card className="w-full h-full border-none shadow-none">
+        <CardHeader className="flex p-7 flex-row items-center gap-x-4 space-y-0">
+          <Button
+            variant="secondary"
+            onClick={onCancel ? onCancel : () => router.back()}
+            size="sm"
+          >
+            <ArrowLeftIcon className="size-4 mr-2" />
+            Back
+          </Button>
+          <CardTitle className="text-xl font-bold">
+            {initialValues.name}
+          </CardTitle>
+        </CardHeader>
+        <div className="px-7">
+          <DottedSeperator />
+        </div>
+        <CardContent className="p-7">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="flex flex-col gap-y-4">
+                <FormField
+                  name="name"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Project Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter Project Name" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  name="image"
+                  control={form.control}
+                  render={({ field }) => (
+                    <div className="flex flex-col gap-y-2">
+                      <div className="flex items-center gap-x-5">
+                        {field.value ? (
+                          <div className="relative w-[72px] h-[72px] rounded-md overflow-hidden">
+                            <Image
+                              src={
+                                field.value instanceof File
+                                  ? URL.createObjectURL(field.value)
+                                  : field.value
+                              }
+                              alt="logo"
+                              fill
+                              className="object-cover"
+                            />
+                          </div>
+                        ) : (
+                          <Avatar className="size-[72px]">
+                            <AvatarFallback>
+                              <ImageIcon className="size-[36px] text-neutral-400" />
+                            </AvatarFallback>
+                          </Avatar>
+                        )}
+                        <div className="flex flex-col">
+                          <p className="text-sm "> Project Icon</p>
+                          <p className="text-sm text-muted-foreground">
+                            JPG, PNG, SVG, or JPEG, max 1MB
+                          </p>
+                          <input
+                            type="file"
+                            className="hidden"
+                            accept=".jpg,.png,.svg,.jpeg"
+                            ref={inputRef}
+                            disabled={isPending}
+                            onChange={handleImageChange}
+                          />
+                          {field.value ? (
+                            <Button
+                              type="button"
+                              variant={"destructive"}
+                              size={"xs"}
+                              onClick={() => {
+                                field.onChange(null);
+                                if (inputRef.current) {
+                                  inputRef.current.value = "";
+                                }
+                              }}
+                              disabled={isPending}
+                              className="w-fit mt-2"
+                            >
+                              Remove Image
+                            </Button>
+                          ) : (
+                            <Button
+                              type="button"
+                              variant={"teritary"}
+                              size={"xs"}
+                              onClick={() => inputRef.current?.click()}
+                              disabled={isPending}
+                              className="w-fit mt-2"
+                            >
+                              Upload Image
+                            </Button>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                />
+              </div>
+              <DottedSeperator className="py-7" />
+              <div className="flex justify-between items-center">
+                <Button
+                  variant={"secondary"}
+                  size={"lg"}
+                  type="button"
+                  onClick={onCancel}
+                  disabled={isPending}
+                  className={cn(!onCancel && "invisible")}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  disabled={isPending}
+                  type="submit"
+                  size={"lg"}
+                  variant={"primary"}
+                >
+                  Save Changes
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+
+      <Card className="w-full h-full border-none shadow-none">
+        <CardContent className="p-7">
+          <div className="flex flex-col">
+            <h3 className=" font-bold">Danger Zone</h3>
+            <p className="text-sm text-muted-foreground">
+              Deleting a project is a irreversible action and will remove all
+              associated data
+            </p>
+            <DottedSeperator className="py-7" />
+
+            <Button
+              className="mt-6 w-fit ml-auto"
+              size="sm"
+              type="button"
+              disabled={isPending}
+              variant="destructive"
+              onClick={() => handleDelete()}
+            >
+              Delete Project
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
