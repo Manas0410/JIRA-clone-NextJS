a6a43542c6ad85170eb13def5ab68882959915b1
Manas0410 <manasshrivastava0410@gmail.com>
2024-11-10 12:28:22 +0530

    project list

--- src/app/(dashboard)/workspaces/[workspaceId]/Client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/Client.tsx
@@ -17,6 +17,8 @@ import { PlusIcon, CalendarIcon, SettingsIcon } from "lucide-react";
 import { formatDistanceToNow } from "date-fns";
 import Link from "next/link";
 import { Card, CardContent } from "@/components/ui/card";
+import { Project } from "@/features/projects/types";
+import { ProjectAvatar } from "@/features/projects/components/projects-avatar";
 
 const Client = () => {
   const workspaceId = useWorkspaceId();
@@ -33,8 +35,6 @@ const Client = () => {
     workspaceId,
   });
 
-  const { open: createProject } = useCreateProjectModal();
-
   const isLoading =
     analyticsLoading || tasksLoading || projectsLoading || membersLoading;
 
@@ -50,6 +50,7 @@ const Client = () => {
       <Analytics data={analytics} />
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
         <TaskList data={tasks.documents} total={tasks.total} />
+        <ProjectList data={projects.documents} total={projects.total} />
       </div>
     </div>
   );
@@ -103,8 +104,58 @@ export const TaskList = ({ data, total }: tasklistProps) => {
           </li>
         </ul>
         <Button variant={"muted"} className="w-full mt-4" asChild>
-          <Link href={`/workspaces/${workspaceId}/tasks`}>Show all</Link>
+          <Link href={`/workspaces/${workspaceId}/tasks`}>
+            More Information
+          </Link>
         </Button>
+      </div>
+    </div>
+  );
+};
+
+interface ProjectlistProps {
+  data: Project[];
+  total: number;
+}
+
+export const ProjectList = ({ data, total }: ProjectlistProps) => {
+  const workspaceId = useWorkspaceId();
+  const { open: createProject } = useCreateProjectModal();
+
+  return (
+    <div className="flex flex-col gap-y-4 col-span-1">
+      <div className="bg-white border rounded-lg p-4">
+        <div className="flex items-center justify-between">
+          <p className="text-lg font-semibold">Projects ({total})</p>
+          <Button variant={"secondary"} size={"icon"} onClick={createProject}>
+            <PlusIcon className="size-4 text-neutral-400" />
+          </Button>
+        </div>
+        <DottedSeperator className="my-4 h-max" />
+        <ul className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+          {data.map((project) => (
+            <li key={project.$id}>
+              <Link href={`/workspaces/${workspaceId}/projects/${project.$id}`}>
+                <Card className="shadow-none rounded-lg hover:opacity-75 transition ">
+                  <CardContent className="p-4 flex items-center  gap-2.5">
+                    <ProjectAvatar
+                      className="size-12"
+                      fallbackClassName="text-lg "
+                      name={project.name}
+                      image={project.imageUrl}
+                    />
+                    <p className="text-lg font-medium truncate">
+                      {project.name}
+                    </p>
+                  </CardContent>
+                </Card>
+              </Link>
+            </li>
+          ))}
+          <li className="text-sm text-muted-foreground text-center hidden first-of-type:block ">
+            No Projects found
+          </li>
+        </ul>
       </div>
     </div>
   );
