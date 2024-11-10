1c52ef7f07065ce4d49f6c5e6def5b59928b2f0d
Manas0410 <manasshrivastava0410@gmail.com>
2024-11-10 23:57:34 +0530

    navbar fixed

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,37 @@
+"use client";
+
 import { UserButton } from "@/features/auth/components/user-button";
 import { MobileSidebar } from "./mobile-sidebar";
+import { usePathname } from "next/navigation";
+import { title } from "process";
+
+const pathNameMap = {
+  tasks: {
+    title: "My Tasks",
+    description: "Monitor all of your tasks here",
+  },
+  projects: {
+    title: "My Projects",
+    description: "Monitor tasks of your projects here",
+  },
+};
+
+const defaultMap = {
+  title: "Home",
+  description: "Monitor all of your project and tasks here",
+};
 
 export const Navbar = () => {
+  const pathname = usePathname();
+  const pathnameParts = pathname.split("/");
+  const pathNameKey = pathnameParts[3] as keyof typeof pathNameMap;
+
+  const { title, description } = pathNameMap[pathNameKey] || defaultMap;
   return (
     <nav className="pt-4 px-8 flex w-full items-center justify-between">
       <div className="hidden flex-col lg:flex">
-        <h1 className="text-2xl font-semibold">Home</h1>
-        <p className="text-muted-foreground">
-          Monitor all of your project and tasks here
-        </p>
+        <h1 className="text-2xl font-semibold">{title}</h1>
+        <p className="text-muted-foreground">{description}</p>
       </div>
       <MobileSidebar />
       <UserButton />
